51aa14c69c989370ab25ae3516fcf2ebb057fd04
jinkee Hong <jinkee.hong329@gmail.com>
2025-01-22 16:43:23 +0900

    refactor

--- Flights.java
+++ Flights.java
@@ -183,7 +183,6 @@ public class Flights implements Airport {
         else
         {
              day =  (rnd.nextInt(31) + 1 ); ;
-            System.err.println("NO SUCH ELEMENTS IS IN LIST");
         }
         return new int[] {year,month,day};
     }
